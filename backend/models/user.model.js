dc202eb17612a1d8e663369a38cdc7c584c6d9f5
Hamza Ilyas <Hamzafullstackdev1@gmail.com>
2024-06-11 20:50:07 +0500

    Design User Schema In Models

---
+++ backend/models/user.model.js
@@ -0,0 +1,65 @@
+import mongoose from "mongoose";
+
+const userScheama = new mongoose.Schema(
+  {
+    fullname: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 6,
+    },
+    followers: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        default: [],
+      },
+    ],
+    following: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        default: [],
+      },
+    ],
+    profilimg: {
+      type: String,
+      default:
+        "https://res.cloudinary.com/gaurav/image/upload/v1631691111/twitter/default-profile-pic_y0q12o.jpg",
+    },
+    coverimg: {
+      type: String,
+      default:
+        "https://res.cloudinary.com/gaurav/image/upload/v1631691111/twitter/default-profile-pic_y0q12o.jpg",
+    },
+    bio: {
+      type: String,
+      default: "",
+    },
+    link: {
+      type: String,
+      default: "",
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userScheama);
+export default User;
