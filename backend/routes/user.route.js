e93a09c25387e7f01328119bbab7b6f75ff568eb
Hamza Ilyas <Hamzafullstackdev1@gmail.com>
2024-06-13 23:00:06 +0500

    Create Suggested Users Api

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { profile, followUnfollowUser } from "../controllers/user.controller.js";
+import { profile, followUnfollowUser, getSuggestedUsers } from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyToken.util.js";
 
 const router = express.Router();
 
 router.get("/profile/:username", verifyToken, profile);
 router.post("/follow/:id", verifyToken, followUnfollowUser);
+router.get("/suggested", verifyToken, getSuggestedUsers);
 
 export default router;
