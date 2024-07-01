d49cf34bb4122dba98f52e0564b3e04f45e08b5f
Hamza Ilyas <Hamzafullstackdev1@gmail.com>
2024-07-01 22:14:09 +0500

    Design The Login Page Ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Signup from "./pages/Signup";
+import Login from "./pages/Login";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/signup' Component={Signup} />
+        <Route path='/login' Component={Login} />
       </Routes>
     </BrowserRouter>
   );
