95c885877df9b6c03a01f5cdbf63417eb061ad10
Hamza Ilyas <Hamzafullstackdev1@gmail.com>
2024-07-01 22:26:10 +0500

    Design The Common Sidebar For All Routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,19 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
+import Home from "./pages/Home";
+import Sidebar from "./components/Sidebar";
 function App() {
   return (
     <BrowserRouter>
+    <div className='flex max-w-6xl mx-auto'>
+    <Sidebar/>
       <Routes>
+        <Route path='/' Component={Home} />
         <Route path='/signup' Component={Signup} />
         <Route path='/login' Component={Login} />
       </Routes>
+		</div>
     </BrowserRouter>
   );
 }
